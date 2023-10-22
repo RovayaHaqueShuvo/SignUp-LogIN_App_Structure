2373a0b916308af2710772fe67cd506e09a4f576
RovayaHaqueShuvo <rovayahaqueshuvo@gmail.com>
2023-10-22 19:55:12 +0600

    first commit

---
+++ app/src/main/java/istt/com/appdevelopment/ParrotInterface.kt
@@ -0,0 +1,11 @@
+package istt.com.appdevelopment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParrotInterface : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parrot_interface)
+    }
+}
